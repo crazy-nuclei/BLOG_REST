3d350b33b498df79ab290202a9d0341fd2adeeed
Aayush Agarwal <crazy_nuclei@Aayushs-MacBook-Air.local>
2023-06-23 13:55:54 +0530

    Handling request to routes that does not exist and  erros generated

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const createErrors = require('http-errors');
 
 const app = express();
 
@@ -13,5 +14,18 @@ app.use(cors({
 app.get('/', (req, res) => {
     res.send("HELLO FROM BLOG APP");
 });
+
+// all the routes that does not exist 
+app.use((req, res, next) => {
+    next(createErrors.NotFound('This route does not exist'));
+})
+
+// handling errors 
+app.use((err, req, res, next) => {
+    res.status(err.status).send({
+        status : err.status || 500, 
+        message : err.message || "Internal Server Error"
+    })
+}) 
 
 module.exports = app;
